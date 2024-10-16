f72c66ead2289fb1f8ce2063c726060c1fc3b23f
Georg Dronov <dronovgeorg72@gmail.com>
2024-10-16 16:22:26 +0300

    add routes for form control

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import pkg from "pg";
 import dotenv from "dotenv";
+import formsRouter from "./routes/forms.js";
 
 dotenv.config();
 
@@ -19,6 +20,8 @@ pool
   .then(() => console.log("Connected to DB"))
   .catch((err) => console.log("Error connecting to DB", err));
 
+app.use("/forms", formsRouter);
+
 app.get("/", (req, res) => {
   res.send("Hello from server.js");
 });
@@ -26,4 +29,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Listening at Port: ${port}`);
 });
-
