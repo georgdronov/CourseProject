3b180852d31928da70564c68a66a6e181ce0be7e
Georg Dronov <dronovgeorg72@gmail.com>
2024-10-21 16:16:19 +0300

    update server db and routes - rework DB

--- server.js
+++ server.js
@@ -1,31 +1,26 @@
 import express from "express";
-import pkg from "pg";
 import dotenv from "dotenv";
-import formsRouter from "./routes/forms.js";
 import cors from "cors";
+import formsRouter from "./routes/forms.js";
+import db from "./db.js"; 
 
 dotenv.config();
 
-const { Pool } = pkg;
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
-});
-
-pool
-  .connect()
-  .then(() => console.log("Connected to DB"))
-  .catch((err) => console.log("Error connecting to DB", err));
-
 app.use("/forms", formsRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello from server.js");
+});
+
+db.pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
 });
 
 app.listen(port, () => {
